c216c07de8b99d0967d6f3dd25aa4c4a5bd3cec4
sayana8 <122979736+sayana8@users.noreply.github.com>
2024-04-14 02:58:50 +0500

    Add files via upload

--- Web_Project/src/app/home/home.component.ts
+++ Web_Project/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  @ViewChild('myHeader')
+  header!: ElementRef;
+  sticky!: number;
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.sticky = this.header.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (window.pageYOffset > this.sticky) {
+      this.header.nativeElement.classList.add('sticky');
+    } else {
+      this.header.nativeElement.classList.remove('sticky');
+    }
+  }
 }
